2e91360096166a85b454be6d2dfa8238c19717ed
tranvanmy <tranvanmydev@gmail.com>
2020-04-22 22:04:25 +0700

    feat(Frontend): Install package babel convert ES6 to ES6

--- server.js
+++ server.js
@@ -1,44 +1,41 @@
-var express = require('express');
-var app = express();
-var bodyParser = require('body-parser');
-var hostname = 'localhost';
-var port = 8017;
+// var express = require('express');
+import express from 'express';
 
-var urlencodedParser = bodyParser.urlencoded({ extended: false });
+let app = express();
+let bodyParser = require('body-parser');
+let hostname = 'localhost';
+let port = 8017;
+
+let urlencodedParser = bodyParser.urlencoded({ extended: false });
 
 app.use(express.static('public'));
 
 app.get("/users", (req, res) => {
-	response = {
-		first_name: req.query.first_name,
-		last_name: req.query.last_name
-	}
-	console.log(response);
-	res.send(JSON.stringify(response));
+    response = {
+        first_name: req.query.first_name,
+        last_name: req.query.last_name
+    }
+    console.log(response);
+    res.send(JSON.stringify(response));
 });
 
 app.delete("/users", (req, res) => {
-	  response = {
-                first_name: req.query.first_name,
-                last_name: req.query.last_name
-        }
-        console.log(response);
-        res.send(JSON.stringify(response));
+    res.send(JSON.stringify(response));
 });
 
 app.post("/users", urlencodedParser, (req, res) => {
-	response = {
-                first_name: req.query.first_name,
-                last_name: req.query.last_name
-        }
-        console.log(response);
-        res.send(JSON.stringify(response));
+    response = {
+        first_name: req.query.first_name,
+        last_name: req.query.last_name
+    }
+    console.log(response);
+    res.send(JSON.stringify(response));
 });
 
 app.get("/", (req, res) => {
-	res.sendFile(__dirname + "/" + "index.html");
+    res.sendFile(__dirname + "/" + "index.html");
 });
 
 app.listen(port, hostname, () => {
-	console.log(hostname, port);
+    console.log(hostname, port);
 });
